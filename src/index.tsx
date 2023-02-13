e7a48e4596394c19b78b4342a7b9461da1405890
EmilsWebbod <emil@webbod.no>
2023-02-13 13:23:37 +0100

    Update exported default class

--- src/index.tsx
+++ src/index.tsx
@@ -9,7 +9,7 @@ import Menu from './core/Menu.js';
 import Content from './core/Content.js';
 import { TranslateSettings } from './types/stat.js';
 
-interface Options {
+export interface IReactTranslateOptions extends TranslateOptions {
   fileServerURL?: string;
   apiServer?: string;
   locales?: LocaleObject;
@@ -36,28 +36,30 @@ function SpawnMenu() {
   );
 }
 
-export default ({ locales, fileServerURL, googleAPIKey, apiServer, settings }: Options) => {
-  const translateSettings = new Settings(locales, fileServerURL, googleAPIKey, Boolean(apiServer));
+export default class ReactTranslate extends Translate {
+  settings: Settings;
 
-  if (apiServer) {
-    new TranslationApi(apiServer);
+  constructor(opts: IReactTranslateOptions) {
+    super(opts);
+    const { locales, fileServerURL, googleAPIKey, apiServer, settings } = opts;
+    this.settings = new Settings(locales, fileServerURL, googleAPIKey, Boolean(apiServer));
+    this.settings.setTranslate(this);
+
+    if (apiServer) {
+      new TranslationApi(apiServer);
+    }
+
+    if (settings) {
+      this.settings.translateSettings = settings;
+    }
+
+    if (typeof document !== 'undefined') {
+      const translateElem = document.createElement('div');
+      translateElem.id = 'react-translate';
+      document.body.appendChild(translateElem);
+      ReactDOM.render(<SpawnMenu />, translateElem);
+    }
   }
+}
 
-  if (settings) {
-    translateSettings.translateSettings = settings;
-  }
-
-  if (typeof document !== 'undefined') {
-    const translateElem = document.createElement('div');
-    translateElem.id = 'react-translate';
-    document.body.appendChild(translateElem);
-    ReactDOM.render(<SpawnMenu />, translateElem);
-  }
-
-  return (props: TranslateOptions) => {
-    const translate = new Translate(props);
-    translateSettings.setTranslate(translate);
-    return translate;
-  };
-};
-export { handleNoMatch, handleNoTranslation };
+export { ReactTranslate, handleNoMatch, handleNoTranslation };
